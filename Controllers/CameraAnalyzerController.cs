9ba61ada290f80ad2a1e7b7e6747a5c6e2908a94
tallavi561 <tallavi561@gmail.com>
2025-10-30 22:02:30 +0200

    feat: add GEMINI API key

--- Controllers/CameraAnalyzerController.cs
+++ Controllers/CameraAnalyzerController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using CameraAnalyzer.bl.Utils;
 using Microsoft.Extensions.Configuration;
+using CameraAnalyzer.bl.APIs;
 
 namespace CameraAnalyzer.Controllers
 {
@@ -8,18 +9,25 @@ namespace CameraAnalyzer.Controllers
     [Route("api1/v1")]
     public class CameraAnalyzerController : ControllerBase
     {
-    public static Logger logger = new Logger();
-    [HttpGet("getHomePage")]
-    public IActionResult GetHomePage()
-    {
-        logger.LogInfo("Home page accessed.");
-        return Ok("HELLO WORLD");
-    }
-    [HttpGet("startProcess")]
-    public IActionResult StartProcess()
-    {
-        logger.LogInfo("Start process is starting.");
-        return Ok("Process Finished");
-    }
+        private readonly GeminiAPI _geminiAPI; // ← שדה פרטי של GeminiAPI
+
+        public static Logger logger = new Logger();
+        public CameraAnalyzerController(GeminiAPI geminiAPI)
+        {
+            _geminiAPI = geminiAPI;
+        }
+        [HttpGet("getHomePage")]
+        public IActionResult GetHomePage()
+        {
+            logger.LogInfo("The API key is: " + _geminiAPI.getApiKey());
+            logger.LogInfo("Home page accessed.");
+            return Ok("HELLO WORLD");
+        }
+        [HttpGet("startProcess")]
+        public IActionResult StartProcess()
+        {
+            logger.LogInfo("Start process is starting.");
+            return Ok("Process Finished");
+        }
     }
 }
---
+++ bl/APIs/GeminiAPI.cs
@@ -0,0 +1,33 @@
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace CameraAnalyzer.bl.APIs
+{
+
+    public class GeminiAPI
+    {
+        private readonly HttpClient _client;
+        private readonly string _apiKey;
+
+        public GeminiAPI(IConfiguration config)
+        {
+            _apiKey = config["GeminiAPI:ApiKey"] ?? throw new Exception("Missing Gemini API key");
+            _client = new HttpClient();
+        }
+
+        public String getApiKey()
+        {
+            return _apiKey;
+        }
+
+        public async Task<string> AskGeminiAsync(string prompt)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, "https://api.gemini.com/v1/query");
+            request.Headers.Add("Authorization", $"Bearer {_apiKey}");
+            request.Content = new StringContent($"{{\"prompt\":\"{prompt}\"}}", System.Text.Encoding.UTF8, "application/json");
+
+            var response = await _client.SendAsync(request);
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
